ad3a9929b43243c97c2c87fe5fc00e744984e01c
yura_zhizhnevskiy <zhizhnevskiy@yandex.ru>
2021-02-21 15:48:35 +0300

    update

--- task01/pdoconfig.php
+++ task01/pdoconfig.php
@@ -1,7 +1,7 @@
 <?php
-$servername = '127.0.0.1';
-$database = 'ost25.11.2020';
-$username = 'root';
-$password = 'root';
+$servername = "127.0.0.1";
+$database = "company_employees";
+$username = "root";
+$password = "";
 $sql = "mysql:host=$servername;dbname=$database;";
 $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
