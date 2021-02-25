7f42e171b5cb98daf969b235fa62d20e83cca49d
Zhiznevskiy_Yura <zhizhnevskiy@yandex.ru>
2021-02-25 09:07:35 +0300

    did

--- task01/public/index.php
+++ task01/public/index.php
@@ -1,17 +1,16 @@
 <?php
 
+error_reporting(E_ALL);
+
 define('PROJECT_DIR', dirname(__DIR__));
 define('SRC_DIR', PROJECT_DIR . DIRECTORY_SEPARATOR . 'src');
-
-error_reporting(E_ALL);
 
 require_once implode(DIRECTORY_SEPARATOR, [PROJECT_DIR, 'config', 'autoloader.php']);
 $config = require_once PROJECT_DIR . '/config/config.php';
 
-//echo '<pre>';
-//var_dump($_SERVER);
 
-echo (new \FlowLearning\Application($config))->handle($_SERVER["REQUEST_URI"]);
+$application = new \FlowLearning\Application($config);
 
+$response = $application->handle($_SERVER["REQUEST_URI"]);
 
-
+echo $response;
